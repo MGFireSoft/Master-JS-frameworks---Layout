eabb94d2264ac144d4bbe1efd47365b46287ef8a
Martin <tinchox00@gmail.com>
2022-09-10 19:04:51 -0300

    installed node.js

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//cargar modulos de node para crear servidor
+
+//ejecutar express (http)
+//middLewares
+
+//CORS
+
+//añadir prefijos a rutas
+
+// exportar modulo (fichero actual)
